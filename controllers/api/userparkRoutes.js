fe99e6b795e3f15f5fcb728c4d825183a0b54473
FractalIceCream <vroco86@gmail.com>
2023-12-20 22:22:19 -0600

    redid userpark creation

--- controllers/api/userparkRoutes.js
+++ controllers/api/userparkRoutes.js
@@ -2,13 +2,30 @@ const router = require('express').Router();
 const { Park, UserPark, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
     const data = await UserPark.findAll();
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json(error);
 
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const park = await Park.findOne({ 
+      where: { park_code: req.body.park_code }
+    });
+
+    const userpark = await UserPark.create({
+      user_id: req.session.user_id,
+      park_id: park.id
+    });
+
+    res.status(200).json("User-Park relationship added");
+  } catch (error) {
+    res.status(500).json(error);    
   }
 })
 
