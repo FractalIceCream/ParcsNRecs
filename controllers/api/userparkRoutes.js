bb677a5b92301bbaa10c7f245ce8aa684f1d34df
FractalIceCream <vroco86@gmail.com>
2023-12-21 18:06:25 -0600

    changed userpark route

--- controllers/api/userparkRoutes.js
+++ controllers/api/userparkRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Park, UserPark, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const data = await UserPark.findAll();
     res.status(200).json(data);
